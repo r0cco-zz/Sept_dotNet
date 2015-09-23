fa3f18aace3ee24228534a8904da58d8fd45be7a
RoccoRiccardi <Rocco.Riccardi.42@gmail.com>
2015-09-23 16:44:21 -0400

    added battleship project for collaboration

---
+++ Battleship/Battleship project/BattleShip_Start/BattleShip.UI/StartMenu.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BattleShip.UI
+{
+    class StartMenu
+    {
+        public static void DisplayStart()
+        {
+            Console.Write("Battleship!! \n\nPlayer 1, What is your name? : ");
+            string player1Name = Console.ReadLine();
+            if (player1Name == "")
+            {
+                player1Name = "Player1";
+            }
+
+            Console.Write("Player 2, what is your name? : ");
+            string player2name = Console.ReadLine();
+            if (player2name == "")
+            {
+                player2name = "Player2";
+            }
+        }
+    }
+}
